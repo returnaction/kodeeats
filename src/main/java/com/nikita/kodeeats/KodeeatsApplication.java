535f9d521dcc4746929d70df6f6363d4ed846ecc
returnaction <obergannv@gmail.com>
2025-09-08 17:29:06 +0300

    Initial commit

---
+++ src/main/java/com/nikita/kodeeats/KodeeatsApplication.java
@@ -0,0 +1,13 @@
+package com.nikita.kodeeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodeeatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodeeatsApplication.class, args);
+    }
+
+}
